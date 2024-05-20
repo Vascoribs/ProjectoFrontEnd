2d04c43a281e18c45610e1c28fe6f6d63c6776c8
Vasco Ribeiro <vascogribeiro@gmail.com>
2024-05-20 14:11:38 +0100

    Criação components e ficheiros jsx necessários. Inicio html header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Headers(){
+    
+    return(
+        <div>
+            <h1>
+                <img src="" alt="" />
+            </h1>
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}

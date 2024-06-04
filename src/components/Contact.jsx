88ba8d3a4454b4b72a329946345cc91358eb1fac
Vasco Ribeiro <vascogribeiro@gmail.com>
2024-06-04 17:11:59 +0100

    Pequenas alteraçoes componente Contact e respectivo CSS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,12 @@ export default function Contact(){
             <h2>Contact Us</h2>
             <form method="post" action="">
                 <div><input type="text" name="name" placeholder="Full Name"/></div>
-                <div><input type="email" name="email" placeholder="Email"/></div>
+                <div><input type="email" name="email" placeholder="Email *" required/></div>
                 <div><input type="text" name="country" placeholder="Country"/></div>
                 <div><input type="text" name="comments"
                 placeholder="Comment" /></div>
-                <div><button type="submit">Contact Us</button></div>
+                <p>* required field</p>
+                <button type="submit">Contact Us</button>
             </form>
         </div>
     )

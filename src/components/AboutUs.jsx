43a8533080d9d6733810d42eef0e7bcda257ccb6
Vasco Ribeiro <vascogribeiro@gmail.com>
2024-06-06 20:38:00 +0100

    Finalização estilização componente AboutUs

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,14 +3,14 @@ export default function AboutUs(){
     return(
         <div className="aboutUs">
             <p>
-                We are OnWest. 
+                We are OnWest.<br /> 
                 We are born from the pure will to move On.
             </p>
             <p> 
-                Whenever things are against us, we move On.
+                Whenever things are against us, we move On.<br />
                 The West is our home.
             </p>
-            <p>Where do we go from here?ON</p>
+            <p>Where do we go from here?<br />ON</p>
         </div>
     )    
 }

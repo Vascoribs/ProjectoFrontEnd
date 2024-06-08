6af4ec7c883d95adbe68b66c758e5106b7111e0f
Vasco Ribeiro <vascogribeiro@gmail.com>
2024-06-08 11:22:00 +0100

    Finalização html componente Shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,6 +1,58 @@
 export default function Shop(){
     
     return(
-        <div>Shop</div>
+        <div className="shop">
+            <h2>Shop</h2>
+            <ol className="shopProducts">
+                <li>
+                    <figure>
+                        <img src="/img/babysweatshirt1.png" alt="" />
+                        <figcaption>Baby Sweatshirt - 30€</figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="/img/cap1.png" alt="" />
+                        <figcaption>Cap - 25€</figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="/img/hoodie1.png" alt="" />
+                        <figcaption>Hoodie - 40€</figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="/img/jacket1.png" alt="" />
+                        <figcaption>Jacket - 50€</figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="/img/shorts1.png" alt="" />
+                        <figcaption>Shorts - 20€</figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="/img/socks1.png" alt="" />
+                        <figcaption>Socks - 10€</figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="/img/tshirt1.png" alt="" />
+                        <figcaption>T-Shirt - 25€</figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="/img/tanktop1.png" alt="" />
+                        <figcaption>Pro Tank - 30€</figcaption>
+                    </figure>
+                </li>
+            </ol>
+        </div>
     )
 }

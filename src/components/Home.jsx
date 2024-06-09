78c54be33933483c3e61a539e9a3120811e39712
Vasco Ribeiro <vascogribeiro@gmail.com>
2024-06-09 21:11:34 +0100

    Actualização Imagens do Projecto

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,29 +10,29 @@ export default function Home(){
                 <ul className="mainImgs">
                     <li>
                         <figure>
-                            <img className="visible" src="/img/cap1.png" alt="" />
-                            <img className="hidden" src="/img/cap2.png" alt="" />      
+                            <img className="visible" src="/img/cap1.jpeg" alt="" />
+                            <img className="hidden" src="/img/cap2.jpeg" alt="" />      
                             <figcaption>Cap - 25€</figcaption>
                         </figure>
                     </li>
                     <li>
                         <figure>
-                            <img className="visible"src="/img/tanktop1.png" alt="" />
-                            <img className="hidden"src="/img/tanktop2.png" alt="" />
+                            <img className="visible"src="/img/tanktop1.jpeg" alt="" />
+                            <img className="hidden"src="/img/tanktop2.jpeg" alt="" />
                             <figcaption>Pro Tank - 30€</figcaption>
                         </figure>
                     </li>
                     <li>
                         <figure id="img3">
-                            <img className="visible" src="/img/shorts1.png" alt="" />
-                            <img className="hidden" src="/img/shorts2.png" alt="" />
+                            <img className="visible" src="/img/shorts1.jpeg" alt="" />
+                            <img className="hidden" src="/img/shorts2.jpeg" alt="" />
                             <figcaption>Shorts - 20€</figcaption>
                         </figure>
                     </li>
                     <li>
                         <figure id="img4">
-                            <img className="visible" src="/img/socks1.png" alt="" />
-                            <img className="hidden" src="/img/socks2.png" alt="" />
+                            <img className="visible" src="/img/socks1.jpeg" alt="" />
+                            <img className="hidden" src="/img/socks2.jpeg" alt="" />
                             <figcaption>Socks - 10€</figcaption>
                         </figure>
                     </li>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -6,50 +6,50 @@ export default function Shop(){
             <ol className="shopProducts">
                 <li>
                     <figure>
-                        <img src="/img/babysweatshirt1.png" alt="" />
+                        <img src="/img/babysweatshirt1.jpeg" alt="" />
                         <figcaption>Baby Sweatshirt - 30€</figcaption>
                     </figure>
                 </li>
                 <li>
                     <figure>
-                        <img src="/img/cap1.png" alt="" />
+                        <img src="/img/cap1.jpeg" alt="" />
                         <figcaption>Cap - 25€</figcaption>
                     </figure>
                 </li>
                 <li>
                     <figure>
-                        <img src="/img/hoodie1.png" alt="" />
-                        <figcaption>Hoodie - 40€</figcaption>
+                        <img src="/img/jacket1.jpeg" alt="" />
+                        <figcaption>Jacket - 45€</figcaption>
                     </figure>
                 </li>
                 <li>
                     <figure>
-                        <img src="/img/jacket1.png" alt="" />
-                        <figcaption>Jacket - 50€</figcaption>
+                        <img src="/img/longsleeve1.jpeg" alt="" />
+                        <figcaption>Long Sleeve - 35€</figcaption>
                     </figure>
                 </li>
                 <li>
                     <figure>
-                        <img src="/img/shorts1.png" alt="" />
+                        <img src="/img/tanktop1.jpeg" alt="" />
+                        <figcaption>Pro Tank - 30€</figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="/img/shorts1.jpeg" alt="" />
                         <figcaption>Shorts - 20€</figcaption>
                     </figure>
                 </li>
                 <li>
                     <figure>
-                        <img src="/img/socks1.png" alt="" />
+                        <img src="/img/socks1.jpeg" alt="" />
                         <figcaption>Socks - 10€</figcaption>
                     </figure>
                 </li>
                 <li>
                     <figure>
-                        <img src="/img/tshirt1.png" alt="" />
+                        <img src="/img/tshirt2.jpeg" alt="" />
                         <figcaption>T-Shirt - 25€</figcaption>
-                    </figure>
-                </li>
-                <li>
-                    <figure>
-                        <img src="/img/tanktop1.png" alt="" />
-                        <figcaption>Pro Tank - 30€</figcaption>
                     </figure>
                 </li>
             </ol>

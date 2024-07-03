56553561e60dcd3c312411c75069cf8825ca316b
Vasco Ribeiro <vascogribeiro@gmail.com>
2024-07-03 01:52:02 +0100

    Inicio trabalho MediaQueries. Finalização breakpoint tablet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,42 +7,60 @@ export default function Header(){
     
     const {cart} = useContext(CartContext);
     const [country, setCountry] = useState("Portugal")
+    
+    const [isHambMenuOpen, setIsHambMenuOpen] = useState(false);
+
+    function openHambMenu() {
+        setIsHambMenuOpen (prevState => !prevState)
+    }
 
     return(
-        <nav> 
-            <h1>
-                <NavLink className="h1" to="/">
-                    <img src="/img/logo_onwest/120x85/OnwestPrancheta1.png" alt="OnWest Track Club" />
-                </NavLink>
-            </h1>
-            <div className="topLeft">
-               <ul>
-                    <li><NavLink className="navbarLink" to="/aboutus">About Us</NavLink></li>
-                    <li><NavLink className="navbarLink" to="/shop">Shop</NavLink></li>
-                    <li><NavLink className="navbarLink" to="/contact">Contact</NavLink></li>
-                </ul>
-            </div>   
-            <div className="topRight">    
-                <select value={ country } onChange={ event => setCountry(event.target.value) }>
-                        <option value="Portugal">Portugal</option>
-                        <option value="Spain">Spain</option>
-                        <option value="France">France</option>
-                        <option value="Italy">Italy</option>
-                        <option value="Netherlands">Netherlands</option>
-                        <option value="UK">UK</option>
-                        <option value="USA">USA</option>
-                </select>
-                <ul className="navButtons">
-                    <li>
-                        <NavLink className="shoppingCartLink" to="/shoppingcart">
-                        <button type="button" name="cartBtn" aria-label="Shopping Cart">
-                                <i className="fa-solid fa-cart-shopping" ></i>
-                        </button>
-                        <div className="cartLength">{cart.length}</div>
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div className="wrapper">
+            <nav> 
+                <h1>
+                    <NavLink className="h1" to="/">
+                        <img src="/img/logo_onwest/120x85/OnwestPrancheta1.png" alt="OnWest Track Club" />
+                    </NavLink>
+                </h1>
+                <div className="topLeft">
+                    <ul>
+                        <li><NavLink className="navbarLink" to="/aboutus">About Us</NavLink></li>
+                        <li><NavLink className="navbarLink" to="/shop">Shop</NavLink></li>
+                        <li><NavLink className="navbarLink" to="/contact">Contact</NavLink></li>
+                    </ul>
+                </div>
+                <div className="hamburgerMenu">
+                    <button type="button" name="hamburguerBtn" aria-label="Menu" onClick={()=>openHambMenu()}>
+                        <i className="fa-solid fa-bars"></i>
+                    </button>
+                        <select>
+                            <option><NavLink className="navbarLink" to="/aboutus">About Us</NavLink></option>
+                            <option><NavLink className="navbarLink" to="/shop">Shop</NavLink></option>
+                            <option><NavLink className="navbarLink" to="/contact">Contact</NavLink></option>
+                        </select>
+                </div>   
+                <div className="topRight">    
+                    <select value={ country } onChange={ event => setCountry(event.target.value) }>
+                            <option value="Portugal">Portugal</option>
+                            <option value="Spain">Spain</option>
+                            <option value="France">France</option>
+                            <option value="Italy">Italy</option>
+                            <option value="Netherlands">Netherlands</option>
+                            <option value="UK">UK</option>
+                            <option value="USA">USA</option>
+                    </select>
+                    <ul className="navButtons">
+                        <li>
+                            <NavLink className="shoppingCartLink" to="/shoppingcart">
+                            <button type="button" name="cartBtn" aria-label="Shopping Cart">
+                                    <i className="fa-solid fa-cart-shopping" ></i>
+                            </button>
+                            <div className="cartLength">{cart.length}</div>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
